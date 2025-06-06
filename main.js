3774ab3d3f1cd5c48fb16b65d81383837ab6f967
Yu <jonathancyw@gmail.com>
2025-06-06 11:13:31 +0800

    changed to using react, beginning ui rewrite

---
+++ main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+    return <body>
+        <div className="app">
+            <Schedule />
+        </div>
+    </body>
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
